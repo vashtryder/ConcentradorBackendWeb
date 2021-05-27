1d338abc2ff6a2766f165330ac879c5d2a3bc970
Manuel Quispe <manuel.quispe@materiagris.pe>
2021-05-27 16:00:16 -0500

    Subir Repositorio Remoto

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  //BASE_ENDPOINT: 'http://dev-concentrador.eastus.azurecontainer.io:8080/api/v1/concentrador',
+  BASE_ENDPOINT: 'https://localhost:44342/api'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
